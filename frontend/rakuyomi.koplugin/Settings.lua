6095a7ee0dd4c5844f959793b931cb71dae7f785
BaconDroid <11067111+BaconDroid@users.noreply.github.com>
2026-09-03 20:13:29 -0400

    fix: standardize title bar button sizes and remove bottom lines
    - Fix padding_bottom on all title bar buttons (was using icon_size, now button_padding)
    - Remove text_font_size from all text buttons (caused crashes)
    - Consistent button_padding = Screen:scaleBySize(11)
    - Move sort button from left to right side in LibraryView (next to search)
    - Move bell button from right to left side in LibraryView (after playlist)
    - Left: settings, playlist, bell+count
    - Right: sort, search, close
    - Remove with_bottom_line from Settings, TrackingSettings, SourceSettings, TaskManagerView

--- frontend/rakuyomi.koplugin/Settings.lua
+++ frontend/rakuyomi.koplugin/Settings.lua
@@ -636,9 +636,6 @@ function Settings:init()
     title = _("Settings"),
     fullscreen = true,
     width = self.dimen.w,
-    with_bottom_line = true,
-    bottom_line_color = Blitbuffer.COLOR_DARK_GRAY,
-    bottom_line_h_padding = padding,
     left_icon = "chevron.left",
     left_icon_tap_callback = function()
       self:onReturn()
